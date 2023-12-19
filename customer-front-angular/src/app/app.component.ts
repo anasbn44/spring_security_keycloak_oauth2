ce294b62e9aa93761ddf82a0b0226b5864fa2126
anasbn44 <banouni.anas0@gmail.com>
2023-12-19 23:58:05 +0100

    commit-4

--- customer-front-angular/src/app/app.component.ts
+++ customer-front-angular/src/app/app.component.ts
@@ -1,22 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SecurityService} from "./services/security.service";
+import {KeycloakService} from "keycloak-angular";
+import {KeycloakProfile} from "keycloak-js";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'customer-front-angular';
-  constructor(public securityService: SecurityService ) {
+  public profile? : KeycloakProfile;
+  constructor(public keycloak:KeycloakService) {
   }
+
+  ngOnInit() {
+    if(this.keycloak.isLoggedIn()){
+      this.keycloak.loadUserProfile().then(profile=>{
+        this.profile=profile;});
+    }
+  }
+
   async login(){
-    await this.securityService.kcService.login({
+    await this.keycloak.login({
       redirectUri : window.location.origin
     })
   }
   onLogout() {
-    this.securityService.kcService.logout(window.location.origin);
+    this.keycloak.logout(window.location.origin);
   }
 
   account() {
